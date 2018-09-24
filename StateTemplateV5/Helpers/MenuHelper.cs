98d1643a77bb4b502d780f4005a50dcbea568ded
konstantin.koryaka <konstantin.koryaka@state.ca.gov>
2018-09-24 11:35:30 -0700

    Fixed active page nav menu autohighlight feature by adding MenuHelper class.

---
+++ StateTemplateV5/Helpers/MenuHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace StateTemplateV5Beta.Helpers
+{
+    public static class MenuHelper
+    {
+        /// <summary>
+        /// Determines whether the specified controller is selected.
+        /// </summary>
+        /// <param name="html">The HTML.</param>
+        /// <param name="controller">The controller.</param>
+        /// <param name="action">The action.</param>
+        /// <returns></returns>
+        public static string IsSelected(this HtmlHelper html, string controller = null, string action = null)
+        {
+            const string cssClass = "active";
+            var currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            var currentController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            if (String.IsNullOrEmpty(controller))
+                controller = currentController;
+
+            if (String.IsNullOrEmpty(action))
+                action = currentAction;
+
+            return controller == currentController && action == currentAction ?
+                cssClass : String.Empty;
+        }
+    }
+}
